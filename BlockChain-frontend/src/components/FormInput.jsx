c91085d121b2880a360d4c5e4bc09d89287cbdf0
Yaswanth1026 <95526015+Yaswanth1026@users.noreply.github.com>
2024-02-21 00:09:00 +0530

    Add files via upload

---
+++ BlockChain-frontend/src/components/FormInput.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import {
+  Box,
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  Textarea,
+  Input,
+} from "@chakra-ui/react";
+const FormInput = ({ label, type, name, onChange, error, register, tag }) => {
+  return (
+    <>
+      <Box mt={5}>
+        <FormControl isInvalid={error}>
+          <FormLabel>{label}</FormLabel>
+
+          {tag === "input" ? (
+            <Input
+              onChange={onChange}
+              type={type}
+              name={name}
+              register={register}
+            />
+          ) : tag === "textarea" ? (
+            <Textarea
+              onChange={onChange}
+              type={type}
+              name={name}
+              register={register}
+            />
+          ) : null}
+
+          {error && (
+            <FormErrorMessage
+              // style={{
+              //   color: "red",
+              // }}
+            >
+              {error.message}
+            </FormErrorMessage>
+          )}
+        </FormControl>
+      </Box>
+    </>
+  );
+};
+
+export default FormInput;
